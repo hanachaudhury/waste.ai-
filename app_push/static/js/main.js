8ef1a68a4437f3d6fd021078f8025d9fb8667d9f
hanachaudhury <34748778+hanachaudhury@users.noreply.github.com>
2018-04-05 17:22:24 -0400

    Update main.js

--- app_push/static/js/main.js
+++ app_push/static/js/main.js
@@ -1,31 +1,32 @@
 // JavaScript source code
 
-// read the image
+// this reads the image that was uploaded by the user 
 $("#file0").on("change", function () {
     let fileReader = new FileReader(),
         fileType = this.files[0].type;
+    //loads the image on click 
     fileReader.onload = function () {
         if (/^image/.test(fileType)) {   
             $("#img0").attr("src", this.result);
             $("#img0").removeClass("hide");
         }
     }
-
+    //displays the file to the user 
     console.log(this.files[0]);
     fileReader.readAsDataURL(this.files[0]);
 });
 
-// click event
+// This is triggered when the user clicks the "Analyze" button in the user interfaction 
 $("#analyze_text").on("click", function () {
     // get the file name of the local image
     var images_file = $("#file0").val();
     var file = images_file.substring(images_file.lastIndexOf('\\') + 1, images_file.length); 
-    if (images_file == "" ) return false;
+    if (images_file == "" ) return false // if the file name is empty nothing is returned 
 
-    // POST the file name get above to python
+    // a POST call is made to retrieve the file name from python 
     $.ajax({
-        url: 'analyze',
-        method: 'POST',
+        url: 'analyze', //url 
+        method: 'POST', //defines the method to get the file 
         data: { 'text': file },
         beforeSend: function () {
             $("#loader").fadeIn();
@@ -35,11 +36,12 @@ $("#analyze_text").on("click", function () {
         success: function (response) {
             $("#loader").fadeOut();
 
-            console.log(response);
+            console.log(response); //displays the response to the user 
 
             $("#results").html("");
             $("#result_text").html("");
-
+            
+            //retrieves the top result from the classifier 
             score = 0;
             classes = 0;
             response['images'][0]['classifiers'][0]['classes'].forEach(function (element, index) {
@@ -57,7 +59,7 @@ $("#analyze_text").on("click", function () {
                 }
                     
             });
-
+            //Displays the confidence level as a percentage for classifying the image 
             $("#result_text").html("<p>Watson is <span class='score'>"
                 + (score * 100).toFixed(2)
                 + "%</span> confident that this waste is <span class= 'class'>"
